8797dba6175cbca969e43e345c4d9c418a035cea
Arslan Khabutdinov <khabutdinov.aa@phystech.edu>
2022-02-26 15:23:24 +0300

    add tagsTree and minor fixes

--- src/components/Common/ProductRating/ProductRating.js
+++ src/components/Common/ProductRating/ProductRating.js
@@ -1,6 +1,5 @@
 import Container from 'react-bootstrap/Container';
 
-<<<<<<< HEAD
 import {calcRating} from 'utils/rating'
 
 import './ProductRating.css'
@@ -12,25 +11,5 @@ export const ProductRating = ({ratings}) => {
         {
             rating.votes === 0 ? <span>No rating yet</span> :  <span> <b>Rating: {rating.rating} </b> (average based on {rating.votes} votes)</span>
         }
-=======
-import './ProductRating.css'
-
-
-export const ProductRating = (props) => {
-    let ratings_agg = props.ratings.reduce((a, b) => (
-        {
-            sum: a.sum + b.rate * b.amount, 
-            amount: a.amount + b.amount
-        }
-    ),
-    {
-        sum: 0,
-        amount: 0
-    }    
-    );
-    let avg_rating = (ratings_agg.sum / ratings_agg.amount).toFixed(2);
-    return <Container className="product-rating">
-        <span> <b>Rating: {avg_rating} </b> (average based on {ratings_agg.amount} votes)</span>
->>>>>>> hw1v1
     </Container>
 }
