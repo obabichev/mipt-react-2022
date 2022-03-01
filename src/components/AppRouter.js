a54f358bcc17d66c7d596cf686337189703beece
Константин Митрофанов <mitrokosta@gmail.com>
2022-03-01 21:21:30 +0300

    Add products page

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
 import {ProductsPage} from "./ProductsPage";
 import {ProductPage} from "./ProductPage";
 
 const AppRouter = () => {
     return <BrowserRouter>
         <Routes>
+            <Route path={"/"} element={<Navigate to={"/products"}/>}/>
             <Route path="/products" element={<ProductsPage/>}/>
             <Route path="/product/:usin" element={<ProductPage/>}/>
         </Routes>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'antd/dist/antd.css';
 import reportWebVitals from './reportWebVitals';
 import AppRouter from "./components/AppRouter";
 
