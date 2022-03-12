2a622a194765af16e06b5654ccf6ea0de44cd26c
Arslan Khabutdinov <khabutdinov.aa@phystech.edu>
2022-03-12 19:57:54 +0300

    one more fix

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -2,11 +2,7 @@ import React from 'react';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {ProductsPage} from "./ProductsPage";
 import {ProductPage} from "./ProductPage";
-<<<<<<< HEAD
 import {ProductEdit} from "./ProductEdit";
-=======
-import {Test} from "./Test";
->>>>>>> 8393ea4c1d2e0bd3db8540ef069b2e880bd9a960
 
 const AppRouter = () => {
     return <BrowserRouter basename="/">
@@ -14,13 +10,9 @@ const AppRouter = () => {
             <Route path="/products" element={<ProductsPage/>}/>
             <Route path="/products/:tag" element={<ProductsPage/>}/>
             <Route path="/product/:usin" element={<ProductPage/>}/>
-<<<<<<< HEAD
             <Route path="/edit" element={<ProductEdit/>}/>
             <Route path="/edit/:usin" element={<ProductEdit/>}/>
             <Route path="*" element={<ProductsPage/>}/>
-=======
-            <Route path="/test" element={<Test/>}/>
->>>>>>> 8393ea4c1d2e0bd3db8540ef069b2e880bd9a960
         </Routes>
     </BrowserRouter>
 }
