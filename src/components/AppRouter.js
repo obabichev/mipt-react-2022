76dd5855d2cddda8807d542954047480f2166f75
obabichev <babichev.oleg.n@gmail.com>
2022-03-17 08:31:10 +0100

    16.03.2022 React hooks

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -3,6 +3,8 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {ProductsPage} from "./ProductsPage";
 import {ProductPage} from "./ProductPage";
 import {Test} from "./Test";
+import {LocalStorageTest} from "./LocalStorageTest";
+import {UseIntervalTest} from "./UseIntervalTest";
 
 const AppRouter = () => {
     return <BrowserRouter>
@@ -10,6 +12,8 @@ const AppRouter = () => {
             <Route path="/products" element={<ProductsPage/>}/>
             <Route path="/product/:usin" element={<ProductPage/>}/>
             <Route path="/test" element={<Test/>}/>
+            <Route path="/lst" element={<LocalStorageTest/>}/>
+            <Route path="/uit" element={<UseIntervalTest/>}/>
         </Routes>
     </BrowserRouter>
 }
--- src/components/Test.js
+++ src/components/Test.js
@@ -1,27 +1,56 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
-const Counter = ({flag}) => {
-    const [value, setValue] = useState(0)
+const Counter = ({flag}) => { // hooks (before changes): [0]
+    console.log("Counter(): was called")
 
-    const handleClick = () => {
-        setTimeout(() => {
+    if (Math.random() < 0.5) {
+        // eslint-disable-next-line react-hooks/rules-of-hooks
+        const [value, setValue] = useState(0)
+        console.log('[obabichev]', {value});
+        const handleClick = () => {
+            setValue(oldValue => oldValue - 1);
+        }
 
-            setValue(oldValue => oldValue + 1);
-        }, 3000)
+        return <div style={{display: flag ? "none" : "block"}}>
+            <button onClick={handleClick}>{value}</button>
+        </div>
+    } else {
+        // eslint-disable-next-line react-hooks/rules-of-hooks
+        const [str, setStr] = useState("")
+        console.log('[obabichev]', {str});
+
+        return <div style={{display: flag ? "none" : "block"}}>
+            <input value={str} onChange={event => setStr(event.target.value)}/>
+        </div>
     }
+}
 
-    return <div style={{display: flag ? "none" : "block"}}>
-        <button onClick={handleClick}>{value}</button>
+const TestUseEffect = (id) => {
+    console.log("TestUseEffect(): was called")
+    const [value, setValue] = useState("")
+
+    const valueToRetriggerUseEffect = value.toUpperCase()
+    useEffect(() => {
+        console.log("TestUseEffect:useEffect", valueToRetriggerUseEffect)
+
+        return () => {
+            console.log("TestUseEffect:useEffect (was cleaned)", valueToRetriggerUseEffect)
+        }
+    }, [valueToRetriggerUseEffect])
+
+    return <div>
+        <input value={value} onChange={event => setValue(event.target.value)}/>
     </div>
 }
 
-export const Test = () => {
+
+
+export const Test = () => { // hooks: [true]
+    console.log("Test(): was called")
     const [flag, setFlag] = useState(true)
 
     return <div>
         <button onClick={() => setFlag(!flag)}>trigger</button>
-        <Counter/>
-        <Counter flag={flag}/>
-        <Counter flag={flag}/>
+        {flag && <TestUseEffect/>}
     </div>
 }
