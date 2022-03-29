25a92f2a2202f60bdfcc8766e7b95dc9e993f164
Константин Митрофанов <mitrokosta@gmail.com>
2022-03-29 21:13:05 +0300

    Add product create form

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,8 @@
 export const countProductMetrics = (product) => {
+    if (!product.ratings) {
+        return [0, 0];
+    }
+
     return product.ratings.reduce(([currentCount, currentSum], {rate, amount}) => {
         return [currentCount + amount, currentSum + rate * amount];
     }, [0, 0]);
