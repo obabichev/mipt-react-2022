ceb8ac14e2b417c5ffdac2c79c160227d0022194
obabichev <babichev.oleg.n@gmail.com>
2022-03-31 23:09:49 +0200

    30.03.2022 Meeting, networking in React

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -5,16 +5,26 @@ import {ProductPage} from "./ProductPage";
 import {Test} from "./Test";
 import {LocalStorageTest} from "./LocalStorageTest";
 import {UseIntervalTest} from "./UseIntervalTest";
+import {TestServerApi} from "./TestServerApi";
+import {QueryClient, QueryClientProvider} from "react-query";
+import {ReactQueryDevtools} from 'react-query/devtools'
+
+
+const queryClient = new QueryClient()
 
 const AppRouter = () => {
     return <BrowserRouter>
-        <Routes>
-            <Route path="/products" element={<ProductsPage/>}/>
-            <Route path="/product/:usin" element={<ProductPage/>}/>
-            <Route path="/test" element={<Test/>}/>
-            <Route path="/lst" element={<LocalStorageTest/>}/>
-            <Route path="/uit" element={<UseIntervalTest/>}/>
-        </Routes>
+        <QueryClientProvider client={queryClient}>
+            <Routes>
+                <Route path="/products" element={<ProductsPage/>}/>
+                <Route path="/product/:usin" element={<ProductPage/>}/>
+                <Route path="/test" element={<Test/>}/>
+                <Route path="/lst" element={<LocalStorageTest/>}/>
+                <Route path="/uit" element={<UseIntervalTest/>}/>
+                <Route path="/test-server-api" element={<TestServerApi/>}/>
+            </Routes>
+            <ReactQueryDevtools initialIsOpen={false}/>
+        </QueryClientProvider>
     </BrowserRouter>
 }
 
